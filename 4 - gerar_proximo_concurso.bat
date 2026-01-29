330a933827fb360bad891d266b15570a4299eab5
Marcelo Henrique <120139095+marceloteck@users.noreply.github.com>
2026-01-29 18:30:02 -0300

    ATUALIZAÇÃO NA QUANTIDADE DE GERAÇÃO

--- 4 - gerar_proximo_concurso.bat
+++ 4 - gerar_proximo_concurso.bat
@@ -34,7 +34,7 @@ echo [INFO] max_sim  : %MAX_SIM%
 echo.
 
 echo [RUN] Gerando jogos (15 e 18) e salvando no banco...
-python "START\gerar_proximo_concurso.py" --size 15 --qtd 12455 --second-size 18 --second-qtd 2 --perfil %PERFIL% --janela %JANELA% --per-brain %PER_BRAIN% --top-n %TOP_N% --max-sim %MAX_SIM% --salvar-db
+python "START\gerar_proximo_concurso.py" --size 15 --qtd 3 --second-size 18 --second-qtd 2 --perfil %PERFIL% --janela %JANELA% --per-brain %PER_BRAIN% --top-n %TOP_N% --max-sim %MAX_SIM% --salvar-db
 
 if errorlevel 1 (
   echo.
